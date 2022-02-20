454936dd9caeed8d8758776c2cb088ca2a9fb084
Alexander Retivov <r.sakha@yandex.ru>
2022-02-20 19:42:27 +0300

    started working on MVC tasks - task1 done and 2nd in progress

---
+++ ASP MVC/task1/WebApplication1/Controllers/MyController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class MyController : Controller
+    {
+        // GET: Home
+        public string Index(string hel)
+        {
+            return ModelClass.ModelHello() + ", " + hel;
+        }
+    }
+}
---
+++ ASP MVC/task2/WebApplication1/Controllers/MyController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class MyController : Controller
+    {
+        // GET: Home
+        public string Index(string hel)
+        {
+            return ExeEnum();
+        }
+
+        public string ExeEnum()
+        {
+            AccountType goldAccount;
+            AccountType platimunAccount;
+            goldAccount = AccountType.Checking;
+            platimunAccount = AccountType.Deposit;
+            string template = "Тип банковского счета {0}";
+            return String.Format(template, goldAccount) + "<p>" + String.Format(template, platimunAccount);
+        }
+    }
+}
