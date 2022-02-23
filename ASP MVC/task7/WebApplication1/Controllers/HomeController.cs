9942b1ffef1b40e6a6f94388837e6c59b7e1f632
Alexander Retivov <r.sakha@yandex.ru>
2022-02-23 21:00:17 +0300

    tasks 7-8

---
+++ ASP MVC/task7/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private CreditContext db = new CreditContext();
+        public ActionResult Index()
+        {
+            GiveCredits();
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult CreateBid()
+        {
+            GiveCredits();
+            List<Bid> allBids = db.Bids.ToList();
+            ViewBag.Bids = allBids;
+
+            return View();
+        }
+
+        [HttpPost]
+        public string CreateBid(Bid newBid)
+        {
+            newBid.BidDate = DateTime.Now;
+            // Добавляем новую заявку в БД
+            db.Bids.Add(newBid);
+            // Сохраняем в БД все изменения
+            db.SaveChanges();
+            return "Спасибо, <b>" + newBid.Name + "</b>, за выбор нашего банка. Ваша заявка будет рассмотрена в течении 10 дней.";
+        }
+
+        private void GiveCredits()
+        {
+            List<Credit> allCredits = db.Credits.ToList();
+            ViewBag.Credits = allCredits;
+        }
+
+        public ActionResult BidSearch(string name)
+        {
+            List<Bid> allBids = db.Bids.Where(a => a.CreditHead.Contains(name)).ToList();
+            if (allBids.Count == 0)
+            {
+                return Content("Указанный кредит " + name + " не найден");
+                //return HttpNotFound();
+            }
+            return PartialView(allBids);
+        }
+    }
+}
---
+++ ASP MVC/task7/WebApplication1/Models/Credit.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Credit
+    {
+        // ID кредита
+        public virtual int CreditId { get; set; }
+        // Название
+        [DisplayName("Название кредита")]
+        public virtual string Head { get; set; }
+        // Период, на который выдается кредит
+        [DisplayName("Период")]
+        public virtual int Period { get; set; }
+        // Максимальная сумма кредита
+        [DisplayName("Размер")]
+        [Required]
+        public virtual int Sum { get; set; }
+        // Процентная ставка
+        [DisplayName("Процентная ставка")]
+        public virtual int Procent { get; set; }
+    }
+}
